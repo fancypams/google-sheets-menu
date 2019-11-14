a68fa18885dfed7c86d65ae0a8930e45fcfaeac1
Pamela <pamrmont@gmail.com>
2019-11-13 23:36:45 -0600

    updated punch and wine styles; updated mobile layout

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 var spreadsheet = "https://docs.google.com/spreadsheets/d/1gOIFQ1HUXtwfqxlUM6mf8ttpTxaoCxHkpxh3Eji0elI/edit#gid=0"
 
 var beerMenu = Handlebars.compile($('#beer-menu').html());
+var punchMenu = Handlebars.compile($('#draft-menu').html());
+var wineMenu = Handlebars.compile($('#wino-menu').html());
 
 $('#bol-menu').sheetrock({
   url: spreadsheet,
   query: "select B,C,D,E,F,G,H where A = 'Section 1'",
-  fetchSize: 11,
+  fetchSize: 6,
   rowTemplate: beerMenu
 });
 
@@ -27,7 +29,21 @@ $('#bol-menu-2').sheetrock({
 $('#bol-menu-3').sheetrock({
   url: spreadsheet,
   query: "select B,C,D,E,F,G,H where A = 'Section 4'",
-  fetchSize: 11,
+  fetchSize: 6,
   rowTemplate: beerMenu,
   num: 31
 });
+
+$('#punch-menu').sheetrock({
+  url: spreadsheet,
+  query: "select B,C,D,E,F,G,H where K = 'punch'",
+  fetchSize: 5,
+  rowTemplate: punchMenu
+})
+
+$('#wine-menu').sheetrock({
+  url: spreadsheet,
+  query: "select B,C,D,E,F,G,H where K = 'wine'",
+  fetchSize: 5,
+  rowTemplate: wineMenu
+})
